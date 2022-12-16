d91b51b443c6719a85648f3e6343541caaff99b0
madhumitha-b <87955195+madhumitha-b@users.noreply.github.com>
2022-12-16 16:12:33 +0530

    Add files via upload

---
+++ 94lo5z/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const curryear = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright @ {curryear} </p>;
+    </footer>
+  );
+}
+export default Footer;
---
+++ 94lo5z/src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the React</h1>
+      <p>
+        React is a free and open-source front-end JavaScript library for
+        building user interfaces based on UI components. It is maintained by
+        Meta and a community of individual developers and companies.
+      </p>
+    </div>
+  );
+}
+export default Note;
